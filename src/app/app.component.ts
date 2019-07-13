2c95ba95db72d84ca55b34a0d3c14a6dfc43f4d5
Jeanheme <Jherreram@sura.com.co>
2019-07-13 12:30:37 -0500

    Adding all the workshop  project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,118 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Observable } from "rxjs";
+
+import { Speaker } from "./interfaces/speaker.interface";
+import {
+  AngularFirestore,
+  AngularFirestoreCollection
+} from "@angular/fire/firestore";
+import { AngularFireStorage } from "@angular/fire/storage";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
 export class AppComponent {
-  title = 'angular-firebase';
+  constructor(
+    private db: AngularFirestore,
+    private storage: AngularFireStorage
+  ) {
+    this.speakerCollection = db.collection("kelly");
+    this.speakerList = this.speakerCollection.valueChanges();
+  }
+
+  speakerCollection: AngularFirestoreCollection<Speaker>;
+  speakerList: Observable<Speaker[]>;
+  
+  @ViewChild("file",{read: false, static: false}) file;
+   files: Set<File> = new Set();
+  
+  url: any =
+      "https://i.pinimg.com/564x/65/df/2c/65df2c922e64c61235162ab7c0924d3c.jpg";
+    _file;
+    name = "";
+    id = null;
+    isChanged = false;
+
+
+
+  onKey(event: any) {
+    // without type info
+    this.name = event.target.value;
+  }
+
+  add() {
+    if (this.isChanged) {
+      const filePath = "workshop" + this.name;
+      const ref = this.storage.ref(filePath);
+      ref.put(this._file).then(() => {
+        ref.getDownloadURL().subscribe(url => {
+          this.url = url;
+          if (this.id) {
+            this.update();
+          } else {
+            this.create();
+          }
+        });
+      });
+    } else {
+      if (this.id) {
+        this.update();
+      }
+    }
+    this.isChanged = false;
+    this.file.nativeElement.value = "";
+  }
+
+  update() {
+    this.speakerCollection
+      .doc(this.id)
+      .update({ name: this.name, url: this.url });
+    this.url =
+      "https://i.pinimg.com/564x/65/df/2c/65df2c922e64c61235162ab7c0924d3c.jpg";
+    this.name = "";
+    this.id = null;
+  }
+
+  create() {
+    const id = this.db.createId();
+    const name = this.name;
+    this.speakerCollection.doc(id).set({ name, url: this.url, id });
+    this.url =
+      "https://i.pinimg.com/564x/65/df/2c/65df2c922e64c61235162ab7c0924d3c.jpg";
+    this.name = "";
+    this.id = null;
+  }
+
+  remove(id: string) {
+    if (confirm("Are you sure to delete the show from your list?")) {
+      this.speakerCollection.doc(id).delete();
+    }
+  }
+
+  select(speaker) {
+    this.url = speaker.url;
+    this.name = speaker.name;
+    this.id = speaker.id;
+  }
+
+  onFilesAdded(target: any) {
+    this.isChanged = true;
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.url = reader.result;
+    };
+    if (target.files.length > 0) {
+      this._file = target.files[0];
+      reader.readAsDataURL(this._file);
+    }
+  }
+
+  addFiles() {
+    this.file.nativeElement.click();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from "@angular/fire/storage";
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
+  declarations: [AppComponent],
+  imports: [ 
     BrowserModule,
-    AppRoutingModule
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyCdWYnqnkZ22kzMHMi3f9bcdioh9I-2dp4",
+      authDomain: "angular-workshop-77d03.firebaseapp.com",
+      databaseURL: "https://angular-workshop-77d03.firebaseio.com",
+      projectId: "angular-workshop-77d03",
+      storageBucket: "angular-workshop-77d03.appspot.com",
+      messagingSenderId: "464255313843",
+      appId: "1:464255313843:web:e8636b76789dcb84"
+    }), 
+    AngularFirestoreModule,
+    AngularFireStorageModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
